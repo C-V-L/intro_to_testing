90c363a6265111dd4fe49c015c13f63c08541f49
Conner Van Loan <cdvanloan@gmail.com>
2022-11-30 09:01:49 -0700

    Adds capitalize method

--- lib/mod.rb
+++ lib/mod.rb
@@ -1,3 +1,5 @@
+require './lib/student'
+
 class Mod
     attr_reader :name,
                 :description,
@@ -6,6 +8,26 @@ class Mod
         @name = name
         @description = description
         @students = []
+        @capitalized_name = []
     end
-    
+
+    def add_student(student)
+        @students << student
+    end
+
+    def capitalized_student_names
+        # capitalized = []
+        
+        # students.each do |student|
+
+        #     capitalized << student.student_name.capitalize
+        # end
+
+        # capitalized
+
+        students.map do |student|
+            student.student_name.capitalize
+        end
+    end
+
 end
--- spec/mod_spec.rb
+++ spec/mod_spec.rb
@@ -1,4 +1,5 @@
 require './lib/mod'
+require './lib/student'
 require 'rspec'
 
 describe Mod do
@@ -19,6 +20,27 @@ describe Mod do
             mod_1 = Mod.new('Mod 1', 'Intro To OOP')
             expect(mod_1.students).to eq([])
         end
+        it 'can add students to mod' do
+            mod_1 = Mod.new('Mod 1', 'Intro To OOP')
+            chris = Student.new('chRIs')
+            richard = Student.new('RiCHard')
+            meg = Student.new('meG')
+            mod_1.add_student(chris)
+            mod_1.add_student(richard)
+            mod_1.add_student(meg)
+            expect(mod_1.students).to include(chris, richard, meg)
+        end
+        it 'can capitalize student names' do
+            mod_1 = Mod.new('Mod 1', 'Intro To OOP')
+            chris = Student.new('chRIs')
+            richard = Student.new('RiCHard')
+            meg = Student.new('meG')
+            mod_1.add_student(chris)
+            mod_1.add_student(richard)
+            mod_1.add_student(meg)
+            expect(mod_1.students).to include(chris, richard, meg)
+            
+
     end
 
 end
