0783e70119cbf1fa0efa2394f88f42c9937d5756
Conner Van Loan <cdvanloan@gmail.com>
2022-11-29 19:36:46 -0700

    Adds new tests to mod

--- spec/mod_spec.rb
+++ spec/mod_spec.rb
@@ -11,6 +11,14 @@ describe Mod do
             mod_1 = Mod.new('Mod 1', 'Intro To OOP')
             expect(mod_1.name).to eq('Mod 1')
           end
+          it 'has a description' do
+            mod_1 = Mod.new('Mod 1', 'Intro To OOP')
+            expect(mod_1.description).to eq('Intro To OOP')
+          end
+        it 'starts with no students' do
+            mod_1 = Mod.new('Mod 1', 'Intro To OOP')
+            expect(mod_1.students).to eq([])
+        end
     end
 
 end
